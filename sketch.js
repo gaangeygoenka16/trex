c110245301fc2b58abb63457cfc29a08b3b517fb
gaangeygoenka16 <85781081+gaangeygoenka16@users.noreply.github.com>
2021-10-08 18:57:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -148,7 +148,7 @@ function spawnClouds() {
 
 function spawnObstacles() {
   if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,height-95,10,40);
+    var obstacle = createSprite(600,height,10,40);
     //obstacle.debug = true;
     obstacle.velocityX = -(6 + 3*score/100);
     
